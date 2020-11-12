926272ef44f46f3a52db316cde5862901b4eeb54
FlutterRaj <rajkar921@gmail.com>
2020-11-12 21:14:54 +0530

    struct examples

--- 14. Struct in c++/school_struct.cpp
+++ 14. Struct in c++/school_struct.cpp
@@ -3,17 +3,40 @@ using namespace std;
 
 struct school
 {
-    int school_id;
-    char name[20];
-    char address[20];
-    
+    private:
+        int id;
+        char name[20];
+        char address[20];
+        int position;
+
+    public:
+        void input()
+        {
+            cout << "Enter School id, name, address, position :: " << endl;
+            cin >> id >> name >> address >> position;
+            if (position < 0)
+                position = position * -1;
+            if(id < 0)
+                id = id * -1;
+        }
+
+        void display()
+        {
+            cout << "School id -> " << id << " name -> " << name << " address -> " << address << " position -> " << position << endl;
+        }
 };
 
 
 int main()
 {
+    school s1, s2, s3;
+    s1.input();
+    s2.input();
+    s3.input();
 
-
+    s1.display();
+    s2.display();
+    s3.display();
 
     return 0;
 }
