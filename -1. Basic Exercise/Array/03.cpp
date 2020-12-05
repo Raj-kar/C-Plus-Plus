bf1ff7d17e2c3588f0434c99ad457bc7ca284775
FlutterRaj <rajkar921@gmail.com>
2020-12-05 11:32:55 +0530

    update logic of 3.cpp

--- -1. Basic Exercise/Array/03.cpp
+++ -1. Basic Exercise/Array/03.cpp
@@ -8,25 +8,33 @@
 #include <iostream>
 using namespace std;
 
+void odd_even(int num, int &even, int &odd)
+{
+    if (num % 2 == 0)
+        even += 1;
+    else
+        odd += 1;
+}
+
 int main()
 {
     int arr1[6], pos = 0, neg = 0, odd = 0, even = 0, zero = 0;
 
     cout << "Enter 20 values " << endl;
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 6; i++)
     {
         cin >> arr1[i];
         if (arr1[i] == 0)
             zero += 1;
         else if (arr1[i] <= 0)
+        {
             neg += 1;
+            odd_even(arr1[i], even, odd);
+        }
         else
         {
             pos += 1;
-            if (arr1[i] % 2 == 0)
-                even += 1;
-            else
-                odd += 1;
+            odd_even(arr1[i], even, odd);
         }
     }
     cout << "number of positive numbers " << pos << endl;
