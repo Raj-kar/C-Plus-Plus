467b35214cdad039c4bdcd3eb728db747e24b165
FlutterRaj <rajkar921@gmail.com>
2020-12-05 11:26:48 +0530

    3 array question

--- -1. Basic Exercise/Array/03.cpp
+++ -1. Basic Exercise/Array/03.cpp
@@ -5,17 +5,35 @@
 ○ number of even numbers
 ○ Number of zeros(0). */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    int arr1[6];
+    int arr1[6], pos = 0, neg = 0, odd = 0, even = 0, zero = 0;
 
     cout << "Enter 20 values " << endl;
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 20; i++)
+    {
         cin >> arr1[i];
-    
+        if (arr1[i] == 0)
+            zero += 1;
+        else if (arr1[i] <= 0)
+            neg += 1;
+        else
+        {
+            pos += 1;
+            if (arr1[i] % 2 == 0)
+                even += 1;
+            else
+                odd += 1;
+        }
+    }
+    cout << "number of positive numbers " << pos << endl;
+    cout << "number of negative numbers " << neg << endl;
+    cout << "number of odd numbers " << odd << endl;
+    cout << "number of even numbers " << even << endl;
+    cout << "Number of zeros(0) " << zero << endl;
 
     return 0;
 }
